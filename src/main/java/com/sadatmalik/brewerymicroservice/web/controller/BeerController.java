77ecd2c55742c43ea1b968e62085eb13d5da4ebd
sadat.malik <sadat.malik@me.com>
2022-02-07 14:39:05 +0000

    Refactoring to v2

--- src/main/java/com/sadatmalik/brewerymicroservice/web/controller/BeerController.java
+++ src/main/java/com/sadatmalik/brewerymicroservice/web/controller/BeerController.java
@@ -9,9 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
-/**
- * Created by jt on 2019-04-20.
- */
+@Deprecated
 @RequestMapping("/api/v1/beer")
 @RestController
 public class BeerController {
---
+++ src/main/java/com/sadatmalik/brewerymicroservice/web/model/v2/BeerStyleEnum.java
@@ -0,0 +1,5 @@
+package com.sadatmalik.brewerymicroservice.web.model.v2;
+
+public enum BeerStyleEnum {
+    LAGER, PILSNER, ALE, STOUT, GOSE, IPA
+}
