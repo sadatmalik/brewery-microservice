c1325a943200a9b6dc5773600160a19d66a7140c
sadat.malik <sadat.malik@me.com>
2022-02-08 12:52:36 +0000

    Adding dates to domain/dto classes and date mapper

--- src/main/java/com/sadatmalik/brewerymicroservice/web/mappers/BeerMapper.java
+++ src/main/java/com/sadatmalik/brewerymicroservice/web/mappers/BeerMapper.java
@@ -4,7 +4,7 @@ import com.sadatmalik.brewerymicroservice.domain.Beer;
 import com.sadatmalik.brewerymicroservice.web.model.BeerDto;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(uses = {DateMapper.class})
 public interface BeerMapper {
 
     BeerDto beerToBeerDto(Beer beer);
